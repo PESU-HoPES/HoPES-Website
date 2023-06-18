2d1045fd5397c39b7bb969169ad6d5323c2f9280
SarangKumar <sarangkumar1578@gmail.com>
2023-06-18 23:51:22 +0530

    minor ui changes

--- app/components/AvatarZoom.jsx
+++ app/components/AvatarZoom.jsx
@@ -1,21 +1,28 @@
+import { AnimatePresence, motion } from 'framer-motion';
 import Image from 'next/image';
 import React from 'react';
 
-const AvatarZoom = ({activePerson}) => {
+const AvatarZoom = ({ activePerson }) => {
     return (
-        <div className='person flex flex-col w-[280px] items-center'>
-            <div className='container rounded-full h-[312px] w-[400px] scale-[0.48] hover:scale-[0.54]'>
-                <div className='container-inner relative -top-[200px] origin-[50%]'>
-                    <div className='circle bg-primary-700 rounded-full h-[380px] left-[10px] cursor-pointer absolute top-[210px] w-[380px]'></div>
-                    <Image
-                        alt={`avatar-${activePerson.id}`}
-                        width={340}
-                        height={400}
-                        className='img img1 pointer-events-none relative left-[22px] top-[164px] w-[340px]'
-                        src={activePerson.zoomImageUrl} />
+        <AnimatePresence>
+            <div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 1, duration: 2 }}
+                className='person flex flex-col w-[280px] items-center'>
+                <div className='container rounded-full h-[312px] w-[400px] scale-[0.48] hover:scale-[0.54]'>
+                    <div className='container-inner relative -top-[200px] origin-[50%]'>
+                        <div className='circle bg-primary-700 rounded-full h-[380px] left-[10px] cursor-pointer absolute top-[210px] w-[380px]'></div>
+                        <Image
+                            alt={`avatar-${activePerson.id}`}
+                            width={340}
+                            height={400}
+                            className='img img1 pointer-events-none relative left-[22px] top-[164px] w-[340px]'
+                            src={activePerson.zoomImageUrl} />
+                    </div>
                 </div>
             </div>
-        </div>
+        </AnimatePresence>
     );
 };
 
--- app/components/TeamSection.jsx
+++ app/components/TeamSection.jsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 
 import React, { useEffect, useState } from 'react';
 import Subnavbar from './Subnavbar';
@@ -7,55 +7,66 @@ import { motion } from 'framer-motion';
 import { filterRoles, teamDetails } from '@/public/constants';
 import AvatarZoom from './AvatarZoom';
 
-
 const TeamSection = () => {
-	const [label, setLabel] = useState('all');
-	const [activePerson, setActivePerson] = useState(null);
-	const [teamDataOriginal, setTeamDataOriginal] = useState(teamDetails);
-	const [teamData, setTeamData] = useState(teamDataOriginal);
+    const [label, setLabel] = useState('all');
+    const [activePerson, setActivePerson] = useState(null);
+    const [teamDataOriginal, setTeamDataOriginal] = useState(teamDetails);
+    const [teamData, setTeamData] = useState(teamDataOriginal);
 
-	useEffect(() => {
-		let filterTeamData = teamDataOriginal.filter((data) => data.categories.includes(label));
-		setTeamData(filterTeamData)
-		// console.log(activePerson);
-	}, [label])
+    useEffect(() => {
+        let filterTeamData = teamDataOriginal.filter((data) =>
+            data.categories.includes(label)
+        );
+        setTeamData(filterTeamData);
+        // console.log(activePerson);
+    }, [label]);
 
-	return (
-		<div>
-			<section className='my-5 flex items-center justify-center'>
-				<Subnavbar setLabel={setLabel} label={label} filterTabs={filterRoles} />
-			</section>
+    return (
+        <div>
+            <section className='my-5 flex items-center justify-center'>
+                <Subnavbar
+                    setLabel={setLabel}
+                    label={label}
+                    filterTabs={filterRoles}
+                />
+            </section>
 
-			<aside className='flex justify-center items-center'>
-				{activePerson !== null
-					? <AvatarZoom activePerson={activePerson} />
-					: <p className='py-2 text-slate-500 text-sm italic'>Select avatar to know more</p>}
-			</aside>
-
-			<motion.section className='w-screen overflow-x-auto no-x-scroll flex gap-x-2 md:gap-x-6'>
-				{teamData.length !== 0 ?
-					teamData.map((detail) => {
-						return (
-							<Avatar
-								key={detail.id}
-								setActivePerson={setActivePerson}
-								categories={detail.categories}
-								id={detail.id}
-								imageUrl={detail.picture}
-								name={detail.name.toLocaleLowerCase()}
-								role={detail.role.toLocaleLowerCase()}
-								domain={detail.domain.toLocaleLowerCase()}
-							/>
-						);
-					})
-					:
-					<p className='w-full items-center py-2 flex justify-center font-lg text-sm italic text-slate-500'>
-						We currenly don&apos;t have people from this domain
-					</p>
+            <aside className='flex items-center justify-center'>
+                {activePerson !== null ? (
+                    <AvatarZoom activePerson={activePerson} />
+                ) : teamData.length !== 0 ? 
+                    <p className='py-2 text-sm italic text-slate-500'>
+                        Select avatar to know more 
+                    </p> : 
+                    ''
+                
 				}
-			</motion.section>
-		</div>
-	)
-}
+            </aside>
 
-export default TeamSection
+            <motion.section className='no-x-scroll flex w-screen gap-x-2 overflow-x-auto py-5 md:gap-x-6'>
+                {teamData.length !== 0 ? (
+                    teamData.map((detail) => {
+                        return (
+                            <Avatar
+                                key={detail.id}
+                                setActivePerson={setActivePerson}
+                                categories={detail.categories}
+                                id={detail.id}
+                                imageUrl={detail.picture}
+                                name={detail.name.toLocaleLowerCase()}
+                                role={detail.role.toLocaleLowerCase()}
+                                domain={detail.domain.toLocaleLowerCase()}
+                            />
+                        );
+                    })
+                ) : (
+                    <p className='font-lg flex w-full items-center justify-center py-2 text-sm italic text-slate-500'>
+                        We currenly don&apos;t have people from this domain
+                    </p>
+                )}
+            </motion.section>
+        </div>
+    );
+};
+
+export default TeamSection;
